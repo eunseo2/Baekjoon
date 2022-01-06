2a7ce9d400a3a4d8163d6a462a06c04443833b68
eunseo2 <20180972@sungshin.ac.kr>
2022-01-06 14:06:37 +0900

    문자열

---
+++ Silver/크로아티아알파벳.java
@@ -0,0 +1,82 @@
+package Silver;
+import java.util.Scanner;
+ 
+public class 크로아티아알파벳 {
+	public static void main(String[] args) {
+    
+		Scanner in = new Scanner(System.in);
+        
+		String str = in.nextLine();
+        
+		int cnt = 0;
+ 
+		for (int i = 0; i < str.length(); i++) {
+ 
+			char ch = str.charAt(i);
+ 
+			if(ch == 'c') {			
+				if(i < str.length() - 1) {
+					if(str.charAt(i + 1) == '=') {		
+						i++;		
+					}
+					else if(str.charAt(i + 1) == '-') {
+						i++;
+					}
+				}
+			}
+		    
+			else if(ch == 'd') {
+				if(i < str.length() - 1) {
+					if(str.charAt(i + 1) == 'z') {
+						if(i < str.length() - 2) {
+							if(str.charAt(i + 2) == '=') {	
+								i += 2;
+							}
+						}
+					}
+		        
+					else if(str.charAt(i + 1) == '-') {	
+						i++;
+					}
+				}
+			}
+		    
+			else if(ch == 'l') {
+				if(i < str.length() - 1) {
+					if(str.charAt(i + 1) == 'j') {
+						i++;
+					}
+				}
+			}
+		    
+			else if(ch == 'n') {
+				if(i < str.length() - 1) {
+					if(str.charAt(i + 1) == 'j') {	
+						i++;
+					}
+				}
+			}
+ 
+			else if(ch == 's') {
+				if(i < str.length() - 1) {
+					if(str.charAt(i + 1) == '=') {	
+						i++;
+					}
+				}
+		    }
+ 
+			else if(ch == 'z') {
+				if(i < str.length() - 1) {
+					if(str.charAt(i + 1) == '=') {
+						i++;
+					}
+				}
+			}
+		    
+			cnt++;
+ 
+		}
+ 
+		System.out.println(cnt);
+	}
+}
