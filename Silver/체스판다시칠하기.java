8e08f62f9623469e3a47e235f584c72f0979b968
eunseo2 <20180972@sungshin.ac.kr>
2022-01-21 15:45:14 +0900

    브루트 포스

---
+++ Silver/체스판다시칠하기.java
@@ -0,0 +1,66 @@
+package Silver;
+
+import java.util.Scanner;
+
+public class 체스판다시칠하기 {
+ 
+	public static boolean[][] arr;
+	public static int min = 64;
+ 
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+ 
+		int N = in.nextInt();
+		int M = in.nextInt();
+ 
+		arr = new boolean[N][M];
+		
+		for (int i = 0; i < N; i++) {
+			String str = in.next();
+			
+			for (int j = 0; j < M; j++) {
+				if (str.charAt(j) == 'W') {
+					arr[i][j] = true;		// W일 때는 true 
+				} else {
+					arr[i][j] = false;		// B일 때는 false
+				} 
+ 
+			}
+		}
+
+		int begin_x = N - 7;
+		int begin_y = M - 7;
+ 
+		for (int i = 0; i < begin_x; i++) {
+			for (int j = 0; j < begin_y; j++) {
+				find(i, j);
+			}
+		}
+		System.out.println(min);
+	}
+ 
+	
+	public static void find(int x, int y) {
+		int end_x = x + 8;
+		int end_y = y + 8;
+		int count = 0;
+ 
+		boolean T = arr[x][y];
+ 
+		for (int i = x; i < end_x; i++) {
+			for (int j = y; j < end_y; j++) {
+
+				if (arr[i][j] != T) {	
+					count++;
+				}
+
+				T = !T;
+			}
+			
+			T = !T;
+		}
+		count = Math.min(count, 64 - count);
+		min = Math.min(min, count);
+	}
+}
