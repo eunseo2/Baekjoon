26d37869129c7011cc9f05d8c44f4f33b3c64620
eunseo2 <20180972@sungshin.ac.kr>
2022-04-11 11:49:50 +0900

    이진탐색

---
+++ Gold/K번째수.java
@@ -0,0 +1,30 @@
+package Gold;
+
+import java.util.Scanner;
+public class K번째수 {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int N = in.nextInt();
+    int K = in.nextInt();
+
+    long left = 1;
+    long right = K;
+
+    while(left < right){
+      long mid = (left+right) /2;
+
+      int cnt = 0;
+      for(int i=1; i<=N; i++){
+        cnt += Math.min(mid/i,N);
+      }
+
+      if(cnt < K){
+        left = mid+1;
+      }else{
+        right = mid;
+      }
+    }
+
+    System.out.println(left);
+  }
+}
